5881918e066913db873c67efab519dbcb6d0a318
KelfinMunene <kelfinmunene@gmail.com>
2025-07-22 21:46:33 +0300

    Submission

---
+++ temp/Maze_Modified_Tuesday.cpp
@@ -0,0 +1,339 @@
+#include "mbed.h"
+
+// pes board pin map
+#include "PESBoardPinMap.h"
+
+// drivers
+#include "DebounceIn.h"
+#include "DCMotor.h"
+#include "SensorBar.h"
+#include "LineFollower.h"
+#include <Eigen/Dense>
+#include <cstdlib>
+#include <cmath>
+
+bool do_execute_main_task = false;
+bool do_reset_all_once = false;
+
+DebounceIn user_button(BUTTON1);
+void toggle_do_execute_main_fcn();
+float getrevolutions(float deg_angle, float wr, float wb);
+bool isIntersection(float, float, float, bool *, bool *, bool *);
+float tanh_approx(float x);
+
+int main()
+{
+    user_button.fall(&toggle_do_execute_main_fcn);
+
+    const int main_task_period_ms = 20; // define main task period time in ms e.g. 20 ms, therefore
+    int counter = 0;
+    // the main task will run 50 times per second
+    Timer main_task_timer;
+
+    // led on nucleo board
+    DigitalOut user_led(LED1);
+    DigitalOut led1(PB_9);
+
+    // --- adding variables and objects and applying functions starts here ---
+
+    /* ***************** DC Motors setup *********************/
+    DigitalOut enable_motors(PB_ENABLE_DCMOTORS);
+    const float voltage_max = 12.0f;
+    const float gear_ratio = 391.00f;
+    const float kn = 36.0f / 12.0f;
+    DCMotor motor_M1(PB_PWM_M1, PB_ENC_A_M1, PB_ENC_B_M1, gear_ratio, kn, voltage_max); // Right
+    DCMotor motor_M2(PB_PWM_M2, PB_ENC_A_M2, PB_ENC_B_M2, gear_ratio, kn, voltage_max); // Left
+
+    const float r_wheel = 0.070f / 2.0f;
+    const float b_wheel = 0.170f;
+    Eigen::Matrix2f Cwheel2robot;
+    Cwheel2robot << r_wheel / 2.0f, r_wheel / 2.0f,
+        r_wheel / b_wheel, -r_wheel / b_wheel;
+    Eigen::Vector2f robot_coord = {0.0f, 0.0f};
+    Eigen::Vector2f wheel_speed = {0.0f, 0.0f};
+
+    // Sensor Bar
+    const float bar_dist = 0.07f; // Distance from wheel axis to sensorbar
+    SensorBar sensor_bar(PB_9, PB_8, bar_dist);
+    float angle = 0.0f;
+    float leftMean = 0.0f, centerMean = 0.0f, rightMean = 0.0f;
+
+    // Controller tuning // TODO Use the non-linear controller later
+    const float Kp = 5.0f;
+    const float kp_nl = 0.05f;
+    const float wheel_vel_max = 2.0f * M_PIf * motor_M2.getMaxPhysicalVelocity();
+
+    // navigation variables
+    bool leftTurn = false, rightTurn = false, crossRoad = false, isIntersection = false , uTurn = false;
+    int dir = 0;
+
+    float finalAngle = 0;
+    float fixedAngle = 0;
+    float get_pos_uturn1 =0;
+    float get_pos_uturn2 =0;
+
+    // set up states for state machine
+    enum RobotState
+    {
+        INITIAL,
+        SLEEP,
+        TRAIN,
+        // RUN,
+        // EMERGENCY
+    } robot_state = RobotState::INITIAL;
+
+    enum MotionState
+    {
+        FOLLOWLINE,
+        PREPARE_TURN,
+        LEFT_TURN,
+        RIGHT_TURN,
+        UTURN,
+        STOP
+    } motion_state = MotionState::FOLLOWLINE;
+
+    // start timer
+    main_task_timer.start();
+
+    // this loop will run forever
+    while (true)
+    {
+        main_task_timer.reset();
+
+        if (counter % 50 == 0)
+        {
+            printf("Robot state: %d, Motion State: %d, counter: %d\n", robot_state, motion_state, counter);
+          
+        }
+
+        if (do_execute_main_task)
+        {
+            led1 = 1;
+            if (sensor_bar.isAnyLedActive())
+                angle = sensor_bar.getAvgAngleRad();
+
+            // Intersection and end detection
+            leftMean = sensor_bar.getMeanThreeAvgBitsLeft();
+            centerMean = sensor_bar.getMeanFourAvgBitsCenter();
+            rightMean = sensor_bar.getMeanThreeAvgBitsRight();
+            printf("left mean: %f right mean: %f center mean: %f \n", leftMean,rightMean,centerMean);
+
+            // --- code that runs when the blue button was pressed goes here ---
+            switch (robot_state)
+            {
+            case RobotState::INITIAL:
+            {
+                // enable hardwaredriver dc motors: 0 -> disabled, 1 -> enabled
+                enable_motors = 1;
+                robot_state = RobotState::TRAIN;
+
+                break;
+            }
+            case RobotState::SLEEP:
+            {
+                // enable hardwaredriver dc motors: 0 -> disabled, 1 -> enabled
+                // enable_motors = 0;
+                if (do_execute_main_task)
+                {
+                    do_execute_main_task = false;
+                    do_reset_all_once = true;
+                    robot_state = RobotState::INITIAL;
+                }
+                break;
+            }
+            case RobotState::TRAIN:
+            {
+                switch (motion_state)
+                {
+                case MotionState::FOLLOWLINE:
+                {
+
+                    leftTurn = (leftMean > 0.5f && centerMean > 0.3f && rightMean <= 0.5f);
+                    rightTurn = (rightMean > 0.5f && centerMean > 0.3f && leftMean <= 0.5f);
+                    crossRoad = (leftMean > 0.5f && centerMean > 0.5f && rightMean > 0.5f);
+                    uTurn = (leftMean < 0.001f && centerMean < 0.001f && rightMean < 0.001f);
+                    isIntersection = (leftTurn || rightTurn || crossRoad);
+                    if (isIntersection)
+                    {
+                        if (crossRoad)
+                        {
+                            dir = -1;
+                        }
+                        else if (leftTurn)
+                            dir = 1;
+                        else
+                            dir = -1;
+
+                        fixedAngle = motor_M1.getRotation() + 0.0f;
+                        finalAngle = motor_M2.getRotation() + 0.0f;
+                        motion_state = MotionState::PREPARE_TURN;
+                    }
+                    else if (uTurn)
+                    {
+                        dir = 2;
+                    }
+                    else
+                    {
+                        // Normal line-following
+                        // Controller implementation
+                        const float w = Kp * angle +  tanh_approx(kp_nl *angle);
+			            const float  v = (0.5f * wheel_vel_max * r_wheel)*(1- tanh_approx(kp_nl*fabsf(angle)));
+                        robot_coord = {v, w};
+                        wheel_speed = Cwheel2robot.inverse() * robot_coord;
+
+                        motor_M1.setVelocity(wheel_speed(0) / (2.0f * M_PIf));
+                        motor_M2.setVelocity(wheel_speed(1) / (2.0f * M_PIf));
+                    }
+                    break;
+                }
+                case MotionState::PREPARE_TURN:
+                {
+                    motor_M1.setRotation(fixedAngle);
+                    motor_M2.setRotation(finalAngle);
+                    
+                    if (std::fabs(motor_M1.getRotation() - fixedAngle) < 0.01)
+                    {
+                        if (dir == -1)
+                        {
+                            // RIGHT 90 DEGREE TURN
+                            fixedAngle = motor_M1.getRotation(); // fixed
+                            finalAngle = motor_M2.getRotation() + getrevolutions(90, r_wheel, b_wheel);
+                            motion_state = MotionState::RIGHT_TURN;
+                        }
+                        else if (dir == 1)
+                        {
+                            // LEFT 90 DEGREE TURN
+                            finalAngle = motor_M1.getRotation() + getrevolutions(90, r_wheel, b_wheel);
+                            fixedAngle = motor_M2.getRotation(); // fixed
+                            motion_state = MotionState::LEFT_TURN;
+                        }
+                        else if (dir == 2)
+                        {
+                            // 180 DEGREE TURN
+                            get_pos_uturn1 = motor_M1.getRotation();
+                            get_pos_uturn2 = motor_M2.getRotation();
+                            finalAngle = motor_M1.getRotation() + getrevolutions(90, r_wheel, b_wheel);
+                            fixedAngle = motor_M2.getRotation() + getrevolutions(-90, r_wheel, b_wheel); // fixed
+                            motion_state = MotionState::UTURN;
+                        }
+                        else
+                            motion_state = MotionState::FOLLOWLINE;
+                    }
+                    break;
+                }
+                case MotionState::LEFT_TURN:
+                {
+                    motor_M1.setRotation(finalAngle);
+                    motor_M2.setRotation(fixedAngle);
+
+                    if (std::fabs(motor_M1.getRotation() - finalAngle) < 0.1)
+                    {
+                        motion_state = MotionState::FOLLOWLINE;
+                    }
+
+                    break;
+                }
+                case MotionState::RIGHT_TURN:
+                {
+
+                    motor_M1.setRotation(fixedAngle);
+                    motor_M2.setRotation(finalAngle);
+
+                    if (std::fabs(motor_M2.getRotation() - finalAngle) < 0.1)
+                    {
+                        motion_state = MotionState::FOLLOWLINE;
+                    }
+                    break;
+                }
+                case MotionState::UTURN:
+                {   
+                    motor_M1.setRotation(get_pos_uturn1);
+                    motor_M2.setRotation(get_pos_uturn2);
+                    thread_sleep_for(1000);
+                    motor_M1.setRotation(fixedAngle);
+                    motor_M2.setRotation(finalAngle);
+
+                    if (std::fabs(motor_M2.getRotation() - finalAngle) < 0.1)
+                    {
+                        motion_state = MotionState::FOLLOWLINE;
+                    }
+
+                    break;
+                }
+                case MotionState::STOP:
+                {
+                    printf("stopping\n");
+                    motor_M1.setVelocity(0);
+                    motor_M2.setVelocity(0);
+
+                    robot_state = RobotState::SLEEP;
+                    break;
+                }
+                default:
+                    break;
+                }
+            }
+            }
+
+            // visual feedback that the main task is executed, setting this once would actually be enough
+        }
+        else
+        {
+            // the following code block gets executed only once
+            motion_state = MotionState::FOLLOWLINE;
+
+            if (do_reset_all_once)
+            {
+                do_reset_all_once = false;
+
+                // --- variables and objects that should be reset go here ---
+
+                // reset variables and objects
+                led1 = 0;
+                robot_state = RobotState::INITIAL;
+                motion_state = MotionState::STOP;
+                counter = 0;
+            }
+        }
+
+        // toggling the user led
+        user_led = !user_led;
+        counter++;
+
+        // --- code that runs every cycle goes here ---
+
+        // read timer and make the main thread sleep for the remaining time span (non blocking)
+        int main_task_elapsed_time_ms = duration_cast<milliseconds>(main_task_timer.elapsed_time()).count();
+        if (main_task_period_ms - main_task_elapsed_time_ms < 0)
+            printf("Warning: Main task took longer than main_task_period_ms\n");
+        else
+            thread_sleep_for(main_task_period_ms - main_task_elapsed_time_ms);
+    }
+}
+
+void toggle_do_execute_main_fcn()
+{
+    // toggle do_execute_main_task if the button was pressed
+    do_execute_main_task = !do_execute_main_task;
+    // set do_reset_all_once to true if do_execute_main_task changed from false to true
+    if (do_execute_main_task)
+        do_reset_all_once = true;
+}
+float getrevolutions(float deg_angle, float wr, float wb)
+{
+    float L = (deg_angle / 360.0f) * 2.0f * M_PIf * wb;
+    return L / (2 * M_PIf * wr);
+}
+
+bool isIntersection(float leftMean, float centerMean, float rightMean, bool *leftTurn, bool *rightTurn, bool *crossRoad)
+{
+    *leftTurn = (leftMean > 0.5f && centerMean > 0.3f && rightMean <= 0.5f);
+    *rightTurn = (rightMean > 0.5f && centerMean > 0.3f && leftMean <= 0.5f);
+    *crossRoad = (leftMean > 0.5f && centerMean > 0.5f && rightMean > 0.5f);
+    bool isIntersection = (leftTurn || rightTurn || crossRoad);
+    return isIntersection;
+}
+
+float tanh_approx(float x) {
+    return x / (1.0f + fabsf(x));
+}
